6c99c68da50d9f6d03f99c6a9e0db8cc631a8de6
luisfelix-93 <lfff6493@gmail.com>
2025-03-18 20:19:56 -0300

    20250318 - #3 parte 2

---
+++ Services/Interfaces/IRoomService.cs
@@ -0,0 +1,14 @@
+﻿using ChatMessAPI.Infrastructure.Entities.DTO;
+using ChatMessAPI.Infrastructure.Entities.Models;
+
+namespace ChatMessAPI.Services.Interfaces
+{
+    public interface IRoomService
+    {
+        Task<ReturnDTO> CreateRoomService(Room pRoom);
+        Task<ReturnDTO> GetRoomListService();
+        Task<ReturnDTO> GetRoomByNameService(string pRoom);
+        Task<ReturnDTO> DeleteRoomByNameService(string pRoom);
+
+    }
+}
--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -2,10 +2,12 @@
 using ChatMessAPI.Infrastructure.Entities.DTO;
 using ChatMessAPI.Infrastructure.Entities.Models;
 using ChatMessAPI.Infrastructure.Repositories.Interfaces;
+using ChatMessAPI.Services.Interfaces;
+using System.Reflection.Metadata;
 
 namespace ChatMessAPI.Services
 {
-    public class RoomService
+    public class RoomService : IRoomService
     {
         #region attributes
         private readonly IRoomRepository _roomRepository;
@@ -56,7 +58,7 @@ namespace ChatMessAPI.Services
                 roomList = await _roomRepository.GetRoomListRepository();
                 if (roomList.Count == 0)
                 {
-                    returnDTO.Message = "Não temos salas criadas";
+                    returnDTO.Message = "Não foi encontrada salas criadas";
                     returnDTO.Success = true;
                     return returnDTO;
                 }
@@ -73,7 +75,62 @@ namespace ChatMessAPI.Services
 
             return returnDTO;
         }
+        public async Task<ReturnDTO> GetRoomByNameService(string pRoom)
+        {
+            ReturnDTO returnDTO = new ReturnDTO
+            {
+                Message = "",
+                Success = false,
+                ResultObject = null
+            };
 
+            RoomEntity room = new RoomEntity();
+
+            try
+            {
+                room = await _roomRepository.GetRoomByNameRepository(pRoom);
+                if (room == null)
+                {
+                    returnDTO.Message = $"Não foi encontrada a sala: {pRoom}";
+                    returnDTO.Success = true;
+                    return returnDTO;
+                }
+
+                returnDTO.Success = true;
+                returnDTO.Message = "Sala encontrada!";
+                returnDTO.ResultObject = room;
+
+            }
+            catch (Exception ex)
+            {
+                returnDTO.Message = $"Erro ao buscar a sala de conversação, {ex.Message}";
+            }
+
+            return returnDTO;
+        }
+
+        public async Task<ReturnDTO> DeleteRoomByNameService(string pRoom)
+        {
+            ReturnDTO returnDTO = new ReturnDTO
+            {
+                Message = "",
+                Success = false,
+                ResultObject = null
+            };
+
+            try
+            {
+                await _roomRepository.DeleteRoomByNameRepository(pRoom);
+                returnDTO.Success = true;
+                returnDTO.Message = "Sala excluida com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                returnDTO.Message = $"Erro excluir a sala de conversação, {ex.Message}";
+            }
+
+            return returnDTO;
+        }
         #endregion
     }
 }
